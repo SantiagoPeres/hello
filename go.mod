d1520ccc8f7700203990d496ec02715afbd5c1fc
SantiagoPeres <santiagogperes@gmail.com>
2022-09-11 20:43:05 +0200

    go workspace

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SantiagoPeres/hello
+
+go 1.18
+
+require github.com/aprendagolang/strutil v0.0.0-20220510213247-adc729752975
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aprendagolang/strutil"
+)
+
+func main() {
+	fmt.Println(strutil.Reverse("Hello"))
+}
